b4617490a2b53997d46a38edd90476080978ab7e
Matadah <85879296+Chanelie@users.noreply.github.com>
2024-01-30 10:35:25 +0200

    Add files via upload

---
+++ css-2024-day1.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jan 29 12:00:10 2024
+
+@author: cmatadah
+"""
+
+print("Chane 2")
+
+
+# three main type of datas
+
+"""
+int
+float
+strings 
+
+"""
